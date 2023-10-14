4800f59446326c028fad4dcc3e547708540e23cc
Sobhitha6 <sobhithapidugu@gmail.com>
2023-10-15 00:04:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Calculator from './calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="calculator">
+        <Calculator />
+      </div>
+    </div>
+  );
+}
+
+export default App;
